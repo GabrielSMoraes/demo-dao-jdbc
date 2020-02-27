60d5f696d065c0f761c20f491bc6dc681654d4f2
Gabriel <gschutzmoraes@gmail.com>
2020-02-27 16:52:58 -0300

    Programa final

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,10 +1,12 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 
-public class Seller implements Serializable{
+public class Seller implements Serializable {
+
 	private static final long serialVersionUID = 1L;
+
 	private Integer id;
 	private String name;
 	private String email;
@@ -12,9 +14,10 @@ public class Seller implements Serializable{
 	private Double baseSalary;
 	
 	private Department department;
-	public Seller () {
-		
+	
+	public Seller() {
 	}
+
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
@@ -23,42 +26,55 @@ public class Seller implements Serializable{
 		this.baseSalary = baseSalary;
 		this.department = department;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getBirthDate() {
 		return birthDate;
 	}
+
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+
 	public Double getBaseSalary() {
 		return baseSalary;
 	}
+
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
+
 	public Department getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(Department department) {
 		this.department = department;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -66,6 +82,7 @@ public class Seller implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -82,10 +99,10 @@ public class Seller implements Serializable{
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-	
 }
